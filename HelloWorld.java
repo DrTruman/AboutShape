3555c966a0743ca40385c8dad73927e69caf8e8a
Pinkarton <kafi@outlook.com>
2023-09-05 21:27:32 -0500

    Hello World Project v1

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+// this program displays the hello world
+// message on the console screen
+
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.println("Hello world");
+        System.out.println("Welcome to Git");
+        
+        Rectangle myRect = new Rectangle(10, 20);
+        System.out.println("Rectangle width is" + myRect.getWidth());
+    }
+} 
---
+++ Rectangle.java
@@ -0,0 +1,18 @@
+// this program displays the hello world
+// message on the console screen
+
+public class Rectangle {
+    public int w, l;
+    
+    public Rectangle (int w, int l){
+        this.w = w;
+        this.l = l;
+    }
+    
+    public int getWidth(){
+        return this.w;
+    }
+    public  int getLength(){
+        return this.l;
+    }
+} 
