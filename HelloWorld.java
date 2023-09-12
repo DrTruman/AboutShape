bda49e79ca54bf9854810a795e5a17c728567b6b
DrTruman <kafi@outlook.com>
2023-09-12 10:41:03 -0500

    new features

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,11 +3,16 @@
 
 public class HelloWorld {
     public static void main(String args[]){
-        System.out.println("Hello world");
+        
         System.out.println("Welcome to Git");
         
         Rectangle myRect = new Rectangle(10, 20);
         System.out.println("Rectangle width is" + myRect.getWidth());
         System.out.println("Rectangle length is" + myRect.getLength());
+
+
+        Rectangle vRect = new Rectangle(10, 20);
+        System.out.println("Rectangle width is" + vRect.getWidth());
+        System.out.println("Rectangle length is" + vRect.getLength());
     }
 } 
